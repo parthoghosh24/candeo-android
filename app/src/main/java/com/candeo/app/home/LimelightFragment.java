7bae1ae8b1200d1b4fcf235cc8d11006934fd943
Partho Ghosh <defjam24@gmail.com>
2015-07-15 11:09:23 +0530

    Working on OOM

--- app/src/main/java/com/candeo/app/home/LimelightFragment.java
+++ app/src/main/java/com/candeo/app/home/LimelightFragment.java
@@ -361,6 +361,7 @@ public class LimelightFragment extends Fragment{
             try {
                 URL imageUrl= new URL(params[0]);
                 BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds=true;
                 options.inPreferredConfig = Bitmap.Config.RGB_565;
                 bitmap = BitmapFactory.decodeStream(imageUrl.openConnection().getInputStream(),null,options);
             } catch (IOException ioe) {
